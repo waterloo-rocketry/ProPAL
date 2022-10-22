45de0a5cc1c3c79aa3e2714630e2030c2941b5c3
Carolx715 <carolx715@gmail.com>
2022-10-22 19:09:33 -0400

    load cantera model from filepath

--- engine_model.py
+++ engine_model.py
@@ -8,14 +8,13 @@ import cantera as ct
 class N2O_HTPB_ThermochemistryModel:
 
     def __init__(self) -> None:
-        self.gas_model = ct.Solution('Mevel2015-rocketry_modified.yaml')
+        self.gas_model = ct.Solution('CanteraModels/Mevel2015-rocketry_modified.yaml')
     
     
     def sim_gas_mixture_combustion_temp(self, OF_ratio, temperature_K, pressure_Pa) -> float:
 
         # This requires a download of a NASA file and some alteration to
         # get it to compile correctly
-
 
         self.gas_model.TPY = temperature_K, pressure_Pa, f'N2O:{OF_ratio}, C4H6:1'
 
