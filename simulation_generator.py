85d96afebb47d6b76446455c32b24245fcc85650
Carolx715 <carolx715@gmail.com>
2022-11-11 15:33:20 -0500

    add more stuff for sim and lookup

--- simulation_generator.py
+++ simulation_generator.py
@@ -6,6 +6,7 @@ from engine_model import CombustionChamberModel, N2O_HTPB_ThermochemistryModel
 import math
 import decimal
 import numpy as np
+from time import perf_counter
 
 from itertools import product
 
@@ -45,16 +46,24 @@ class SimulationGenerator:
       temp = []
       # get precision 
       precision = self.get_precision(val[2])
-      for x in np.linspace(val[0], val[1], int((val[1]-val[0])/val[2] + 1)):
+      # for x in np.linspace(val[0], val[1], int((val[1]-val[0])/val[2] + 1)): 
+      for x in np.arange(val[0], val[1]+val[2], val[2]):
         temp.append(round(x, precision))
       iters.append(temp)
-      # writer.writerow(temp)
 
+    prev_value = iters[0][0]
+    last_value = iters[0][-1]
+
+    start_time = perf_counter()
     for item in product(*iters):
-      # result = cantera_function(*item)
-      writer.writerow([*item])
+      if item[0] > prev_value:
+        prev_value = item[0]
+        print(prev_value, '/', last_value)
+      result = cantera_function(*item)
+      writer.writerow([*item, result.P, result.cv, result.cp, result.T])
 
     print("writing done")
+    print('Cantera simulation generator time: ' + str(perf_counter() - start_time))
     f.close()
 
     
@@ -70,16 +79,37 @@ class SimulationGenerator:
 
 
 class LookUpTable:
-  def __init__(self) -> None: 
+  def __init__(self, filename) -> None: 
+    f = open(filename, 'r')
+    self.reader = csv.reader(f)
+    for row in self.reader:
+      print(row)
+    f.close()
     pass
+
+  def lookup(self, params_to_search):
+    # perform trilinear interpolation
+    pass
+
+class Sim:
+  def __init__(self, lookup_table_path=None) -> None:
+    if lookup_table_path == None:
+      self.generate_from_lookup = False
+    else:
+      self.generate_from_lookup = True
+    pass
+  
+
 
 sg = SimulationGenerator('.')
 
 stuff = {
-  'OF_ratio': [1.000, 5.000, .2],
+  'OF_ratio': [1.000, 5.000, .3],
   'temperature_K': [290, 310, 0.5], 
-  'pressure_Pa': [10000, 20000, 100]
+  'pressure_Pa': [10000, 20000, 250]
 }
 
 sg.generate_lookup(stuff, N2O_HTPB_ThermochemistryModel.sim_gas_mixture_combustion_temp)
 # sg.remove_lookup(stuff)
+
+# lut = LookUpTable("./SG-OF_ratio-temperature_K-pressure_Pa---1.0-5.0-0.3-290-310-0.5-10000-20000-100.csv")
